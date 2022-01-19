8501fd6c4905f23693d6bdc0e9687ba20a78cb38
Mercedes <mercedeswagner84@gmail.com>
2022-01-19 15:05:33 +0100

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let title = document.getElementById("title");
+
+console.log(title);
+
+title.classList.add("styleTitle");
+
+
+let list = document.getElementById("list");
+
+console.log(list);
+
+list.classList.add("styleList");
